4c25fc603a72843c158809e242eb7923f1abae69
Amitkr1311 <1311amitkr@gmail.com>
2026-01-26 03:12:23 +0530

    04:authentication ui

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,13 @@
+import { SignUpView } from '@/modules/auth/ui/sign-up-views'
+import Layout from '../Layout'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <Layout>
+      <SignUpView />
+    </Layout>
+  )
+}
+
+export default Page
